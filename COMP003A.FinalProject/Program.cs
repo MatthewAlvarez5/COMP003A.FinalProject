ea025de0831c246a71b75c8a68fa928e1ad20af4
MatthewAlvarez5 <alvarezmatthew2002@gmail.com>
2023-03-09 21:34:27 -0800

    Questions

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -13,16 +13,14 @@ namespace COMP003A.FinalProject
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
-            string birthYearWord;
-            string gender;
-            int age;
-            int birthYear;
-            
-
-            firstName = wordAnswerLoop("Dagoth: What is your given name?: ");
-            lastName = wordAnswerLoop("Dagoth: What is your family name?: ");
+            string firstName; string lastName; string birthYearWord; string gender;
+            string species; string hairColor; string eyeColor; string heightWord; string weightWord;
+            string hometown; string fightYears; string weapon; string fighterType;
+            char genderLetter; char allegiance;
+            int age; int birthYear; int height; int weight; int timeYears;
+            /*
+            firstName = wordAnswerLoop("Dagoth: What is your given name?: ", "Dagoth: What foreign tongue is that? I'm gonna need it my language this time.");
+            lastName = wordAnswerLoop("Dagoth: What is your family name?: ", "Dagoth: What foreign tongue is that? I'm gonna need it my language this time.");
             if(firstName == lastName)
             {
                 Console.WriteLine($"Dagoth: {firstName} {lastName}? What kinda name is that? Your parents are bloody loons!  ");
@@ -35,8 +33,34 @@ namespace COMP003A.FinalProject
             } while (!checkBirthYear(birthYear));
             age = 2023 - birthYear;
             
-            
-            
+            Console.WriteLine("Dagoth: “And I hope I don’t have to remind ya about your gender”");
+            Console.ReadLine();
+            do
+            {
+                gender = wordAnswerLoop("Dagoth: “What is your gender?” (M, F, or O) \nYou: ", "Dagoth: What foreign tongue is that? I'm gonna need it my language this time.");
+                if (checkCharacter(gender))
+                {
+                    gender = gender.ToLower();
+                    if (gender == "m" || gender == "f" || gender == "o")
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Dagoth: “You dont have to lie about it. Just tell me what you are.”");
+                }
+            } while (true);
+            genderLetter = Convert.ToChar(gender);
+            */
+            species = wordAnswerLoop("Dagoth: “Who are your peoples?” (Species: Ex. Human, Elf, Orc, Giant, etc.). ", "Dagoth: Never heard o' them before, I think your lyin'.");
+            hairColor = wordAnswerLoop("Dagoth: “Your hair color… you don’t see many people around here with… that color. What was it called again?”", "No, no that's not it it was somethin' else.");
+            eyeColor = wordAnswerLoop("Dagoth: “Well, the color of your eyes are… it’s hard to see with this little light. What color are those?” ", "No, no that's not it it was somethin' else.");
+            Console.WriteLine($"Dagoth: “{eyeColor}… I see it now. Now that we are standing eye to eye it seems that you’re a wee bit taller than me. ");
+            heightWord = numberAnswerLoop("Dagoth: “What is your height? In inches of course.”");
+            height = Convert.ToInt16(heightWord);
+
+
 
         }
         /// <summary>
@@ -44,7 +68,7 @@ namespace COMP003A.FinalProject
         /// </summary>
         /// <param name="question">string input</param>
         /// <returns></returns>
-        static string wordAnswerLoop(string question)
+        static string wordAnswerLoop(string question, string error)
         {
             string answer;
             while (true)
@@ -57,7 +81,7 @@ namespace COMP003A.FinalProject
                 }
                 else
                 {
-                    Console.WriteLine("What foreign tongue is that? I'm gonna need it my language this time.");
+                    Console.WriteLine(error);
                 }
             }
         }
@@ -79,7 +103,7 @@ namespace COMP003A.FinalProject
                 }
                 else
                 {
-                    Console.WriteLine("Oi! I asked for a number not whatever hell it is you gave me!");
+                    Console.WriteLine("Oi! I asked for a number! Do you need me to ask louder?");
                 }
             }
         }
@@ -110,6 +134,32 @@ namespace COMP003A.FinalProject
             string pattern = "^[0-9]+$";
             if (Regex.IsMatch(number, pattern))
             {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Checks if the input is a single character
+        /// </summary>
+        /// <param name="question">string input</param>
+        /// <returns></returns>
+        static bool checkCharacter(string answer)
+        {
+            
+            string pattern = "^[A-Za-z]+$";
+            if (Regex.IsMatch(answer, pattern))
+            {
+                try
+                {
+                    char letter = Convert.ToChar(answer);
+                }
+                catch
+                {
+                    return false;
+                }
                 return true;
             }
             else
